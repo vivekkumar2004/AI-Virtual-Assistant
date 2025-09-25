593531a89b46f6878e0250d2b9c96dbaac7a31e1
vivek kumar verma <vivek23vermasrivastava@gmail.com>
2025-09-25 17:33:58 +0530

    setup database connection and create User model

---
+++ Backend/config/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connectDb = async ()=>
+{
+    try {  
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log("db connected");
+        
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+export default connectDb;
--- Backend/index.js
+++ Backend/index.js
@@ -1,18 +1,17 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import connectDb from './config/db.js'
 
 dotenv.config()
 
 const app = express()
 const port = process.env.PORT || 5000 
 
-app.get('/',(req,res)=>
-{
-    res.send("Hi")
-})
+
 
 app.listen(port,()=>
 {
+    connectDb();
     console.log(`Server is started`);
     
 })
