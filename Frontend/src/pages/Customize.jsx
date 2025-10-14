07073904cecbed869a1da259f0cb0a2756b21c67
vivek_verma <149783816+vivekkumar2004@users.noreply.github.com>
2025-10-14 12:07:05 +0530

    Update Customize.jsx

--- Frontend/src/pages/Customize.jsx
+++ Frontend/src/pages/Customize.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useRef, useState } from "react";
 import { IoArrowBackOutline } from "react-icons/io5";
-import Card from "../components/card";
+import Card from "../components/Card";
 import image1 from "../assets/image1.png";
 import image2 from "../assets/image2.jpg";
 import image4 from "../assets/image4.png";
